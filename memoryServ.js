51632c9d637378edbf1179e433a5ce9d94b44e9b
Jeremy Gaudry <jgaudry1294@gmail.com>
2023-04-07 01:12:44 +0200

    cours dev js

--- memoryServ.js
+++ memoryServ.js
@@ -1,2 +1,9 @@
-var table = document.querySelector("table");
-console.log(table);
+var table = document.querySelector("#tableauLivres");
+
+var livre1 = document.createElement("tr");
+livre1.innerHTML = `<td>L'algorithmique selon H2PROG</td>
+                <td>Matthieu GASTON</td>
+                <td>200</td>
+                <td><button class="btn btn-warning">Modifier</button></td>
+                <td><button class="btn btn-danger">Supprimer</button></td>`;
+table.appendChild(livre1);
